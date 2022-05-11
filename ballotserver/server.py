38c1ac20bdddac5711901b9a50815271ed331f7d
Lennart Lopin <info@marscoin.org>
2022-05-11 16:17:52 +0000

    b server change

--- ballotserver/server.py
+++ ballotserver/server.py
@@ -1,17 +1,77 @@
 #!/usr/bin/env python
 
+# WS server example that synchronizes state across clients
+
 import asyncio
-import datetime
-import random
+import json
+import logging
 import websockets
+import ssl
 
-async def time(websocket, path):
-    while True:
-        now = datetime.datetime.utcnow().isoformat() + 'Z'
-        await websocket.send(now)
-        await asyncio.sleep(random.random() * 3)
+logging.basicConfig()
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
 
-start_server = websockets.serve(time, '0.0.0.0', 3678)
+# Generate with Lets Encrypt, copied to this location, chown to current user and 400 permissions
+ssl_cert = "/etc/letsencrypt/live/martianrepublic.org/fullchain.pem" 
+ssl_key =  "/etc/letsencrypt/live/martianrepublic.org/privkey.pem"
+
+ssl_context.load_cert_chain(ssl_cert, keyfile=ssl_key)
+
+STATE = {"value": 0}
+
+USERS = set()
+
+
+def state_event():
+    return json.dumps({"type": "state", **STATE})
+
+
+def users_event():
+    return json.dumps({"type": "users", "count": len(USERS)})
+
+
+async def notify_state():
+    if USERS:  # asyncio.wait doesn't accept an empty list
+        message = state_event()
+        await asyncio.wait([user.send(message) for user in USERS])
+
+
+async def notify_users():
+    if USERS:  # asyncio.wait doesn't accept an empty list
+        message = users_event()
+        await asyncio.wait([user.send(message) for user in USERS])
+
+
+async def register(websocket):
+    USERS.add(websocket)
+    await notify_users()
+
+
+async def unregister(websocket):
+    USERS.remove(websocket)
+    await notify_users()
+
+
+async def counter(websocket, path):
+    # register(websocket) sends user_event() to websocket
+    await register(websocket)
+    try:
+        await websocket.send(state_event())
+        async for message in websocket:
+            data = json.loads(message)
+            if data["action"] == "minus":
+                STATE["value"] -= 1
+                await notify_state()
+            elif data["action"] == "plus":
+                STATE["value"] += 1
+                await notify_state()
+            else:
+                logging.error("unsupported event: {}", data)
+    finally:
+        await unregister(websocket)
+
+
+start_server = websockets.serve(counter, "0.0.0.0", 3678, ssl=ssl_context)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
