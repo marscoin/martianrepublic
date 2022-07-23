330a6a0a60fc98c91888e113002f5d8246c798a3
Lennart Lopin <novalis78@gmail.com>
2022-07-23 11:59:14 -0400

    basic logbook beta

--- resources/views/logbook/myentries.blade.php
+++ resources/views/logbook/myentries.blade.php
@@ -17,14 +17,14 @@
                     <tr>
                         <td class="valign-middle"><?=$pub->ipfs_hash?> <a target="_blank" href="https://ipfs.marscoin.org/ipfs/<?=$pub->ipfs_hash?>"> <i class="fa fa-globe"> <i class="fa fa-external-link"></a></td>
                         <td class="valign-middle"><?=$pub->created_at?></td>
-                        <td class="valign-middle"><?=$pub->notarization?></td>
+                        <td class="valign-middle"><a target="_blank" href="<?=$pub->notarization?>" ><?=substr($pub->notarization, 0, 6)?>...</a></td>
                         <?php if($pub->notarization){ ?>
                         <td class="file-info valign-middle">
                             <span class="label label-default demo-element public-status">Notarized</span>
                         </td>
                         <?php }else{ ?>
                             <td class="file-info valign-middle">
-                            <a data-toggle="modal" href="#notarizemeModal" rel="<?=$pub->ipfs_hash?>" class="btn-lg btn-success demo-element notarizemeModalBtn">Notarize...</a>
+                            <a data-toggle="modal" href="#notarizemeModal" rel="<?=$pub->ipfs_hash?>" class="btn btn-success demo-element notarizemeModalBtn">Notarize...</a>
                             </td>
                         <?php } ?>
                     </tr>
@@ -49,12 +49,15 @@
             <div class="modal-body">
                 <div class="modal-body-box">
                     <h5> Document: </h5>
-                    <p class="modal-document"></p>
+                    <p class="modal-document" style="font-size: 18px; font-weight: 800;"></p>
+                    <h5> Fee: </h5>
+                    <p class="modal-fee" style="font-size: 18px; font-weight: 800;"></p> MARS
                 </div>
                 <div class="modal-message" style="display: none">
                     <i class="fa fa-times-circle"></i>
                     <span id="modal-message-error" style="color:red; font-weight: 600"> </span>
                     <span id="modal-message-success" style="font-weight: 600"> </span>
+                    <a  rel="noreferrer noopener"  target="_blank"  class="transaction-hash-link" href=""> </a>
                 </div>
             </div>
             <h5 class="transaction-hash" style="text-align: center;"></h5>
