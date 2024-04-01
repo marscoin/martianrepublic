e54ecac46fb67371efa857d8ecea8e3bde59efb2
Lennart Lopin <novalis78@gmail.com>
2024-04-01 15:59:04 -0400

    mobile wallet model missing in API class

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\HDWallet;
 
 class User extends Authenticatable
 {
@@ -55,7 +56,7 @@ class User extends Authenticatable
 
     public function hdWallet()
     {
-        return $this->hasOne(HdWallet::class, 'user_id');
+        return $this->hasOne(HDWallet::class, 'user_id');
     }
 
 }
