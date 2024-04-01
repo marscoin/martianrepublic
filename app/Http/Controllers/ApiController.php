61b811d8767f3181a35b035813571cdd4ea2fd04
Lennart Lopin <novalis78@gmail.com>
2024-04-01 16:28:40 -0400

    model update

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -21,7 +21,7 @@ class ApiController extends Controller
     {
         $perPage = 25;
         $feeds = Feed::with(['user' => function ($query) {
-            $query->select('id', 'fullname', 'email', 'created_at');
+            $query->select('id', 'fullname', 'created_at');
         }, 'user.profile' => function ($query) {
             $query->select('userid', 'general_public'); // only select general_public and the foreign key
         }])
@@ -37,12 +37,16 @@ class ApiController extends Controller
     public function allCitizen()
     {
         $perPage = 25;
-        $feeds = Feed::with(['user.profile'])
-            ->whereHas('user.profile', function ($query) {
-                $query->where('tag', 'CT');
-            })
-            ->orderByDesc('id')
-            ->paginate($perPage);
+        $feeds = Feed::with(['user' => function ($query) {
+            $query->select('id', 'fullname', 'created_at');
+        }, 'user.profile' => function ($query) {
+            $query->select('userid', 'general_public'); // only select general_public and the foreign key
+        }])
+        ->whereHas('user.profile', function ($query) {
+            $query->where('tag', 'CT');
+        })
+        ->orderByDesc('id')
+        ->paginate($perPage);
     
         return response()->json($feeds);
     }
@@ -51,14 +55,26 @@ class ApiController extends Controller
     public function allApplicants()
     {
         $perPage = 25; 
-    
+
         $applicants = User::whereHas('profile', function ($query) {
-                $query->where('has_application', 1);
-            })
-            ->with(['profile', 'hdWallet']) 
-            ->orderByDesc('id')
-            ->paginate($perPage);
-    
+            $query->where('has_application', 1);
+        })
+        ->with([
+            'profile' => function ($query) {
+                // Assuming 'userid' is needed for relation mapping
+                // and you want to show 'general_public' from profiles
+                $query->select('userid', 'general_public');
+            },
+            'hdWallet' => function ($query) {
+                // Select the 'user_id' for relation mapping 
+                // and any other fields you want to include from hd_wallet
+                $query->select('user_id', 'public_addr');
+            }
+        ])
+        ->select('id', 'fullname', 'created_at') // Only selecting the fields we want from User
+        ->orderByDesc('id')
+        ->paginate($perPage);
+
         return response()->json($applicants);
     }
 }
