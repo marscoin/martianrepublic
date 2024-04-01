215443a9f328eb79b3cb5931f073cd5a43f22aa1
Lennart Lopin <novalis78@gmail.com>
2024-04-01 17:47:01 -0400

    added showCitizen

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::get('/feed/public', 'App\Http\Controllers\ApiController@allPublic')->name
 Route::get('/feed/citizen', 'App\Http\Controllers\ApiController@allCitizen')->name('api_allcitizen');
 Route::get('/feed/applicant', 'App\Http\Controllers\ApiController@allApplicants')->name('api_allapplicants');
 
+Route::get('/citizen/{address}', 'App\Http\Controllers\ApiController@showCitizen')->name('api_show');
