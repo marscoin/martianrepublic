274c47850d29a08c977ea252bcd61ab0b5cde986
Lennart Lopin <novalis78@gmail.com>
2024-04-04 11:55:26 -0400

    404 fix

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -393,9 +393,14 @@ li a:hover {
           </li>
 
         </ul>
+
       </nav>
+
     </div> 
+
   </header>
+
+        
         <div class="stars">
             <div class="central-body">
                 <img class="image-404" src="/assets/landing/img/404.svg" width="300px">
