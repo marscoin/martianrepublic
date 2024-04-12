462a4db50c4814ab1e7886a408748cd4505aa16f
Lennart Lopin <novalis78@gmail.com>
2024-04-12 11:16:58 -0400

    files

---
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -0,0 +1,11 @@
+<tr>
+<td>
+<table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
+<tr>
+<td class="content-cell" align="center">
+© 2024 The Marscoin&trade; Foundation, Inc. All rights reserved.
+</td>
+</tr>
+</table>
+</td>
+</tr>
