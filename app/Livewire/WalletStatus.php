7bd711dbbc0d526536bad8a80577ef8f4303e89b
Lennart Lopin <novalis78@gmail.com>
2024-04-12 15:46:47 -0400

    speed improvement

--- app/Livewire/WalletStatus.php
+++ app/Livewire/WalletStatus.php
@@ -3,7 +3,9 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Profile; 
 use App\Models\CivicWallet; 
+use App\Models\HDWallet; 
 use App\Includes\AppHelper;
 use Illuminate\Support\Facades\Auth;
 
@@ -11,16 +13,36 @@ class WalletStatus extends Component
 {
     public $wallet_open = false;
     public $balance = 0;
-    public $uid; 
+    public $uid;
+    public $loading = true;
+
+    public function loadWalletData()
+    {
+        $user = Auth::user();
+        $profile = Profile::where('userid', '=', $user->id)->first();
+
+        if ($profile && $profile->wallet_open > 0) {
+            $wallet = HDWallet::where('user_id', '=', $user->id)->first();
+        } elseif ($profile && $profile->civic_wallet_open > 0) {
+            $wallet = CivicWallet::where('user_id', '=', $user->id)->first();
+        } else {
+            $wallet = null;
+        }
+
+        if ($wallet) {
+            $this->wallet_open = true;
+            $this->balance = AppHelper::getMarscoinBalance($wallet->public_addr);
+        } else {
+            $this->wallet_open = false;
+            $this->balance = 0;
+        }
+
+        $this->loading = false;
+    }
+
     public function mount()
     {
-        $civic_wallet = CivicWallet::where('user_id', '=', Auth::id())->first();
-        $balance = AppHelper::getMarscoinBalance($civic_wallet->public_addr);
-
-        if ($civic_wallet) {
-            $this->wallet_open = true;
-            $this->balance = $balance; 
-        }
+        $this->loading = true;
     }
 
     public function render()
--- resources/views/livewire/wallet-status.blade.php
+++ resources/views/livewire/wallet-status.blade.php
@@ -1,20 +1,31 @@
-@if ($wallet_open)
-<div class="wallet-is-open" >
-    <h5 class="nav-wallet-balance"
-        style="background-color: #2f4354; color: white; padding: 9px; border-radius: 8px; text-align: center">
-        <img src="/assets/wallet/img/marscoin-350x350.png" width="20" height="20" />
-        {{ $balance }} MARS
-    </h5>
+<div wire:poll.60s="loadWalletData" wire:init="loadWalletData">
+    @if ($loading)
+    <div class="wallet-is-open" >
+            <h5 class="nav-wallet-balance"
+                style="background-color: #2f4354; color: white; padding: 9px; border-radius: 8px; text-align: center">
+                <img src="/assets/wallet/img/marscoin-350x350.png" width="20" height="20" />
+                <i class="fa fa-spinner fa-spin fa-fw"></i> MARS
+            </h5>
+        </div>
+    @else
+        @if ($wallet_open)
+        <div class="wallet-is-open" >
+            <h5 class="nav-wallet-balance"
+                style="background-color: #2f4354; color: white; padding: 9px; border-radius: 8px; text-align: center">
+                <img src="/assets/wallet/img/marscoin-350x350.png" width="20" height="20" />
+                {{ $balance}} MARS
+            </h5>
+        </div>
+        @else
+        <div class="wallet-is-not-open" >
+            <a href="/wallet/dashboard/hd" style="text-decoration:none; color: white" class="connect-wallet">
+                <h5 style="background-color: #2f4354; color: white; padding: 9px; border-radius: 8px; text-align: center"
+                    class="nav-wallet-connect">
+                    <img src="/assets/wallet/img/marscoin-350x350.png" width="20" height="20" />
+                    Connect Wallet
+                </h5>
+            </a>
+        </div>
+        @endif
+    @endif
 </div>
-@else
-{{-- The commented out div can be used or removed based on your need --}}
-<div class="wallet-is-not-open" >
-    <a href="/wallet/dashboard/hd" style="text-decoration:none; color: white" class="connect-wallet">
-        <h5 style="background-color: #2f4354; color: white; padding: 9px; border-radius: 8px; text-align: center"
-            class="nav-wallet-connect">
-            <img src="/assets/wallet/img/marscoin-350x350.png" width="20" height="20" />
-            Connect Wallet
-        </h5>
-    </a>
-</div>
-@endif
