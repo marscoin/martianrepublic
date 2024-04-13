e7165fe72c549a8553799dd5973f4055d868f044
Lennart Lopin <novalis78@gmail.com>
2024-04-12 20:13:16 -0400

    small fix

--- resources/views/congress/noteligableyet.blade.php
+++ resources/views/congress/noteligableyet.blade.php
@@ -37,7 +37,7 @@
                 </nav>
             </div>
         </header>
-        @include('wallet.mainnav', array('active'=>'congress', 'balance' => $balance))
+        @include('wallet.mainnav', array('active'=>'congress')
         <div class="content">
             <div class="container">
                 <div class="portlet">
