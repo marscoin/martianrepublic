4dcaa4fd4271e1be3e623eb843585d4630bb4b40
Lennart Lopin <novalis78@gmail.com>
2024-04-30 11:18:36 -0400

    privacy policy

---
+++ resources/views/privacy.blade.php
@@ -0,0 +1,342 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+<head>
+  <title>Martian Republic</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,400italic,600,600italic,800,800italic">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald:400,300,700">
+  <link href="https://fonts.googleapis.com/css2?family=Courier+Prime:wght@700&family=Orbitron:wght@500&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="/assets/landing/css/font-awesome.min.css">
+  <link rel="stylesheet" href="/assets/landing/css/bootstrap.min.css">
+  <link href="/assets/landing/css/mvpready-landing.css" rel="stylesheet">
+  <link href="/assets/landing/css/mvpready-flat.css" rel="stylesheet">
+  <link href="/assets/landing/css/animate.css" rel="stylesheet">
+  <link rel="shortcut icon" href="/assets/favicon.ico">
+  <style>
+    .mini-feature-title{
+      font-size: 25px;
+    }
+    .shadow-lg {
+        box-shadow: 0 1rem 3rem rgb(0 0 0 / 18%) !important;
+    }
+    .lead-small{
+      font-size: 19px;
+    }
+    .divider-wider{
+      margin-bottom: 110px;
+      margin-top: 50px;
+    }
+  </style>
+</head>
+<body class=" ">
+<div id="wrapper">
+  <header class="navbar navbar-inverse" role="banner">
+
+    <div class="container">
+
+      <div class="navbar-header">
+        <a href="/" class="navbar-brand navbar-brand-img"  style="font-family: 'Orbitron', sans-serif;">
+          <img style="width: 67px;" src="/assets/landing/img/logomarscoinwallet.png" alt="MVP Ready">
+        Martian Republic
+        </a>
+
+        <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <i class="fa fa-bars"></i>
+        </button>
+      </div> <!-- /.navbar-header -->
+
+
+      <nav class="collapse navbar-collapse" role="navigation">
+
+        <ul class="nav navbar-nav navbar-right mainnav-menu">
+
+          <li class="">
+            <a target="_blank" href="https://marscoin.gitbook.io/marscoin-documentation/">Documentation</a>
+          </li>
+
+          <li>
+            <a href="/signup">Signup</a>
+          </li>
+
+          <li>
+            <a href="/login">Login</a>
+          </li>
+
+        </ul>
+
+        <ul class="nav navbar-nav navbar-social navbar-left">
+
+          <li>
+            <a target="_blank" href="http://facebook.com/marscoin" class="ui-tooltip" title="Facebook" data-placement="bottom">
+              <i class="fa fa-facebook"></i>
+              <span class="navbar-social-label">Facebook</span>
+            </a>
+          </li>
+
+          <li>
+            <a target="_blank" href="http://twitter.com/marscoinorg" class="ui-tooltip" title="Twitter" data-placement="bottom">
+              <i class="fa fa-twitter"></i>
+              <span class="navbar-social-label">Twitter</span>
+            </a>
+          </li>
+
+          <li>
+            <a target="_blank" href="https://discord.gg/6vVKH6QdYb" class="ui-tooltip" title="Discord" data-placement="bottom">
+              <i class="fa fa-github-alt"></i>
+              <span class="navbar-social-label">Discord</span>
+            </a>
+          </li>
+
+          <li>
+            <a target="_blank" href="https://reddit.com/r/marscoin" class="ui-tooltip" title="Reddit" data-placement="bottom">
+              <i class="fa fa-reddit"></i>
+              <span class="navbar-social-label">Reddit</span>
+            </a>
+          </li>
+
+        </ul>
+
+      </nav>
+
+    </div> <!-- /.container -->
+
+  </header>
+
+
+
+     <div class="masthead">
+      
+      <div class="container">
+
+        <h1 class="masthead-subtitle">
+          Privacy Policy
+        </h1>
+    
+      </div> <!-- /.container -->
+    </div> <!-- /.masthead -->
+
+
+    <div class="content">
+      
+      <div class="container">        
+
+        <div class="row">
+
+          <div class="col-md-7">
+
+            <h2 class="no-bold">Introduction <strong></strong></h2>
+
+            <p class="lead">Welcome to the Martian Congressional Republic, an innovative, blockchain-based governance system operated by and proprietary to the Marscoin Foundation, Inc. By visiting, accessing, or using our services, you consent to the policies and practices described in this Privacy Policy, so please review them thoroughly. Should you find any policies or practices described here unacceptable, please refrain from using our services. References to "we", "us", or "our" in this Privacy Policy refer to the Marscoin Foundation, Inc. and any of its affiliates.</p>
+
+            <br class="xs-30">
+            <h2 class="no-bold">General Terms <strong></strong></h2>
+            <p>This Privacy Policy outlines the types of information we collect, the reasons for collecting it, and how we handle your information. Your privacy is important to us, so whether you are new to the Martian Congressional Republic or a long-time participant, please take the time to get to know our practices. We encourage you to keep a copy of this Privacy Policy, but note that we may modify these policies at any time. You can always find the latest version here on this page. Continued use of our services after changes means you accept those changes. If you do not agree to this Privacy Policy, please do not use our services..</p>
+
+            <br class="xs-30">
+            <h2 class="no-bold">Open Source Participation<strong></strong></h2>
+            <p>The entire Martian Congressional Republic project, including all associated tools and the technology stack, is hosted publicly on GitHub at github.com/marscoin/martianrepublic. All code is available under the MIT License, which permits free use, including the rights to copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the software. We actively encourage and invite the public to engage with our codebase—whether by copying, forking, or spawning derivative works, or by contributing improvements. The Marscoin™ Foundation is enthusiastic about partnerships and collaborative projects that advance the technologies and tools designed to help humanity settle Mars and the broader Solar System. We welcome all who wish to contribute to these efforts and are excited to explore collaborative possibilities that enhance our mission.</p>
+
+            <br class="xs-30">
+            <h2 class="no-bold">Digital Assets <strong></strong></h2>
+            <p>As used here, “Digital Asset” refers to Marscoin and other blockchain-based assets used within our system, all governed by cryptographic protocols which  are decentralized, and open source. These assets are used as a medium of exchange and store of value within our community.</p>
+
+            <br class="xs-30">
+            <h2 class="no-bold">Governance and Voting System <strong></strong></h2>
+            <p>The Martian Congressional Republic establishes a pioneering form of governance utilizing an end-to-end encrypted, cryptographically secure, and fully auditable voting system. This system necessitates the establishment of a voter registry that collects minimal Personally Identifiable Information (PII). The collection of this data is crucial for the basic functioning of our governance model and is shared openly with all members of the community. However, it is important to note that the submission of this data is entirely voluntary, as is the level of participation in our system. We gather no additional data beyond what is necessary for participation in the voting process, ensuring that our members maintain control over their personal information and their involvement.</p>
+
+            <br class="xs-30">
+            <h2 class="no-bold">Information We Collect <strong></strong></h2>
+            <p>
+            <ul><li><strong>Device Information</strong>: Information automatically collected about your device, such as hardware, operating system, and browser.</li><li><strong>Location Information</strong>: Information automatically collected via analytics to determine your location, including IP addresses and domain names.</li><li><strong>Log Information</strong>: Information generated by your use of our services automatically collected and stored in our logs.</li><li><strong>Account Information</strong>: Information from your account activities such as transactions and balances.</li><li><strong>Correspondence</strong>: Information you provide in communications with us regarding account management and support.</li></ul>
+            </p>
+
+            <br class="xs-30">
+            <h2 class="no-bold">Cookies <strong></strong></h2>
+            <p>
+            We use cookies and similar tracking technologies on your device to help recognize you as a user, enhance your user experience by customizing our services and content, and ensure your account security by detecting irregular activities. If you block or delete cookies, some features may not be available. If you do not consent to cookies, please do not use our services.
+            </p>
+
+            <br class="xs-30">
+            <h2 class="no-bold">Use of Information <strong></strong></h2>
+            <p>
+            The information we collect is used to enhance your experience, protect against fraud, and improve our services. We may share information with trusted third-party service providers who assist us in operating our platform. These providers are bound by strict confidentiality obligations.
+            </p>
+            
+            <br class="xs-30">
+            <h2 class="no-bold">Sharing and Disclosure <strong></strong></h2>
+            <p>
+            We do not sell your personal information. We may share your information with law enforcement or regulatory agencies if required. We use your information to provide and improve our services, conduct security and fraud prevention checks, and support customer service.
+            </p>
+            
+            <br class="xs-30">
+            <h2 class="no-bold">Data Accuracy and Retention <strong></strong></h2>
+            <p>
+            We take steps to ensure your information is accurate and retained only as long as necessary for the purposes for which it was collected, unless further retention is required by law.
+            </p>
+
+
+            <br class="xs-30">
+            <h2 class="no-bold">Access and Correction of Information <strong></strong></h2>
+            <p>
+            You have the right to access and correct your information. For requests, please contact us at info@marscoin.org. A fee may apply for data access requests to cover administrative costs.
+            </p>
+
+            <br class="xs-30">
+            <h2 class="no-bold">Questions<strong></strong></h2>
+            <p>If you have questions or concerns about our privacy practices, please contact us at info@marscoin.org.
+            </p>
+
+            <p>By using the Martian Congressional Republic services, you acknowledge and agree to this Privacy Policy.</p>
+            
+          </div> <!-- /.col -->
+
+
+          <div class="col-md-5">
+
+            <br class="xs-30 sm-0">
+
+            <div class="heading-block">
+              <h4>Be Your Future</h4>
+            </div>
+
+            <img src="/assets/landing/img/sidebar.png" style="width: 250px;">
+
+
+            <br class="xs-60">
+
+            
+          </div> <!-- /.col -->
+
+        </div> <!-- /.row -->
+
+      </div> <!-- /.container -->
+
+    </div>
+
+
+
+
+
+</div> <!-- /#wrapper -->
+
+<footer class="footer">
+
+	<div class="container">
+
+      <div class="row">
+
+        <div class="col-sm-3">
+          <h4 class="content-title">
+            <span>Martian Republic</span>
+          </h4>
+
+          Is a project by the Marscoin Foundation, Inc. to further the cause of Marscoin and cryptocurrencies in space exploration.
+        </div> <!-- /.col -->
+
+
+        <div class="col-sm-3">
+
+          <h4 class="content-title">
+            <span>Twitter Feed</span>
+             <a class="twitter-timeline" height="250" href="https://twitter.com/marscoinorg" data-chrome="nofooter  noscrollbar" data-widget-id="492843006043516928">Tweets by @marscoinorg</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+          </h4>
+        </div> <!-- /.col -->
+
+
+        <div class="col-sm-3">
+
+          <h4 class="content-title">
+            <span>Stay Connected</span>
+          </h4>
+
+          <p></p>
+
+          <br>
+
+          <ul class="footer-social-link">
+            <li>
+              <a href="javascript:;" class="ui-tooltip" title="Facebook" data-placement="bottom">
+                <i class="fa fa-facebook"></i>
+              </a>
+            </li>
+            <li>
+              <a href="javascript:;" class="ui-tooltip" title="Twitter" data-placement="bottom">
+                <i class="fa fa-twitter"></i>
+              </a>
+            </li>
+            <li>
+              <a href="javascript:;" class="ui-tooltip" title="Google+" data-placement="bottom">
+                <i class="fa fa-google-plus"></i>
+              </a>
+            </li>
+          </ul>
+
+        </div> <!-- /.col -->
+
+
+        <div class="col-sm-3">
+
+        <h4 class="content-title">
+          <span>Stay Updated</span>
+        </h4>
+
+        <p>Get emails about new theme launches &amp;  future updates.</p>
+
+        <form action="/" class="form">
+
+          <div class="form-group">
+            <!-- <label>Email: <span class="required">*</span></label> -->
+            <input class="form-control" id="newsletter_email" name="newsletter_email" type="text" value="" required="" placeholder="Email Address">
+          </div> <!-- /.form-group -->
+
+          <div class="form-group">
+            <button class="btn btn-transparent">Subscribe Me</button>
+          </div> <!-- /.form-group -->
+
+        </form>
+
+      </div> <!-- /.col -->
+
+      </div> <!-- /.row -->
+
+	</div> <!-- /.container -->
+
+</footer>
+
+<footer class="copyright">
+  <div class="container">
+
+    <div class="row">
+
+      <div class="col-sm-12">
+        <p>Copyright &copy; 2014-<?=date('Y')?> The Marscoin Foundation, Inc.</p>
+      </div> <!-- /.col -->
+
+    </div> <!-- /.row -->
+
+  </div>
+</footer>
+
+
+<script src="/assets/landing/js/libs/jquery-1.10.2.min.js"></script>
+<script src="/assets/landing/js/libs/bootstrap.min.js"></script>
+
+<script src="/assets/landing/js/plugins/timeago/jquery.timeago.js"></script>
+<script src="/assets/landing/js/plugins/tweetable/tweetable.jquery.min.js"></script>
+<script src="/assets/landing/js/plugins/carouFredSel/jquery.carouFredSel-6.2.1-packed.js"></script>
+
+<script src="/assets/landing/js/mvpready-core.js"></script>
+<script src="/assets/landing/js/mvpready-landing.js"></script>
+
+
+</body>
+</html>
