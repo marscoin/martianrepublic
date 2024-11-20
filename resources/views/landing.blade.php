b3a0928e0915e0e038d61facb5ec82e36538b5cd
Lennart Lopin <lennart@bytefederal.com>
2024-11-20 15:50:04 -0500

    ios banner

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -480,7 +480,8 @@
               <p class="lead lead-small">Step into the future of Martian governance with our mobile app, an extension of the MartianRepublic.org platform. This app integrates a non-custodial wallet and public voter registry with a coin-shuffle based, encrypted ballot distribution system, ensuring that every vote on public proposals, bills, and amendments is secure and private.</p>
               <p class="lead lead-small">Our purely on-chain, cryptographically secure voting process enables each citizen to participate directly in shaping a new society. The Martian Congress, formed by the very public it serves, operates without intermediaries, making governance fully transparent and directly accountable to its citizens.</p>
               <p class="lead lead-small">The app uses the Marscoin blockchain to timestamp every legislative proposal and securely register citizen identities, enhancing trust and transparency in the electoral process. With its intuitive interface, the app allows all participants to easily audit and verify the validity of each vote.</p>
-              <p class="lead lead-small">Embracing an open source ethos, the app's code serves as the constitution. This groundbreaking approach ensures that every citizen can propose, amend, and vote on legislation directly from their device.</p>
+              <p class="lead lead-small">Embracing an open source ethos, the app's code serves as the constitution. This groundbreaking approach ensures that every citizen can propose, amend, and vote on legislation directly from their device. </p>
+              <p>Best of all: it's all open source and soon on Mars!</p>
               <br>
               <a target="_blank" data-aos="fade-up" data-aos-delay="150" href="https://apps.apple.com/us/app/martianrepublic/id6480416861" class="btn btn-default">Download</a>
           </div>
